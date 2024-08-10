9942de16a28ca72237859aecd24ed94ad99aa6a3
Oula Mardawi <u5580340@live.warwick.ac.uk>
2024-08-10 08:11:37 +0000

    add edge cases

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -9,9 +9,10 @@ from urllib.parse import urljoin
 import tldextract
 
 counter = 0
+counter2 = 0
 
 # Step 1: Read the Excel file
-file_path = 'C:/Users/u5580340/Desktop/DS1-U.xlsx'  # Replace with your file path
+file_path = 'C:/Users/u5580340/Desktop/DS1.xlsx'  # Replace with your file path
 df = pd.read_excel(file_path, header=None)
 
 # Step 2: Convert to a list of lists
@@ -32,11 +33,21 @@ def get_first_half(text):
 def extract_main_domain(url):
     # Extract the main domain
     extracted = tldextract.extract(url)
-    main_domain = f"{extracted.domain}.{extracted.suffix}"
-        # Ensure the URL starts with 'https://'
-    if not  main_domain.startswith(('http://', 'https://')):
-         main_domain = 'https://' +  main_domain
+    
+    # Determine if the suffix is a country code
+    suffix_parts = extracted.suffix.split('.')
+    if len(suffix_parts) > 1 and len(suffix_parts[-1]) == 2:
+        # If the last part of the suffix is a two-letter country code, remove it
+        main_domain = f"{extracted.domain}.{suffix_parts[-2]}"
+    else:
+        main_domain = f"{extracted.domain}.{extracted.suffix}"
+    
+    # Ensure the URL starts with 'https://'
+    if not main_domain.startswith(('http://', 'https://')):
+        main_domain = 'https://' + main_domain
+    
     return main_domain
+
 
 def get_full_url(base_url, link):
     # Ensure the link is a full URL
@@ -45,10 +56,9 @@ def get_full_url(base_url, link):
 def extract_owner_from_url(url):
     print("extract_owner_from_url")
     prompt = f"""
-        You are an expert in domain owner extraction. Visit the provided domain URL and find the data controller or owner information. Look for details such as the name of the data controller, owner, or any contact information related to data protection.
-
+        You are an expert in domain owner extraction. 
         ### Instructions:
-        1. Identify the name of the data controller or owner.
+        1. Identify the name of the data controller or owner from given URL.
         2. If the owner is an abbreviation, PROVIDE the full name.
         3. If the owner is a platform, provide the top-level owner or manager, not sub-entities.
         4. If there is a larger entity or different name associated with the company, also provide this information.
@@ -63,7 +73,7 @@ def extract_owner_from_url(url):
 """
 # Using the updated endpoint
     response = client.chat.completions.create(
-        model="gpt-4o-mini",
+        model="gpt-4o",
         messages=[
             {"role": "system", "content": "You are an assistant that extracts owner of any domain."},
             {"role": "user", "content": prompt}
@@ -121,7 +131,52 @@ def extract_owner_from_text(text):
         print(f"An error occurred")
         first_half = get_first_half(text)
         return extract_owner_from_text(first_half)
-   
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------   
+def get_about_us_link(soup, url):
+    # Define keywords to search for "About Us" links
+    keywords = ['about us', 'about', 'our story', 'who we are']
+
+    # Search for links with the keywords in their text or href attribute
+    for keyword in keywords:
+        # Search by text
+        link = soup.find('a', string=lambda text: text and keyword in text.lower())
+        if link:
+            about_us_url = get_full_url(url, link['href'])
+            return about_us_url
+
+        # Search by href
+        link = soup.find('a', href=lambda href: href and keyword in href.lower())
+        if link:
+            about_us_url = get_full_url(url, link['href'])
+            return about_us_url
+        
+
+    return None
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+def get_contact_us_link(soup, url):
+    # Define keywords to search for "Contact Us" links
+    keywords = ['contact us', 'contact', 'get in touch', 'reach us', 'support']
+
+    # Search for links with the keywords in their text or href attribute
+    for keyword in keywords:
+        # Search by text
+        link = soup.find('a', string=lambda text: text and keyword in text.lower())
+        if link:
+            contact_us_url = get_full_url(url, link['href'])
+            return contact_us_url
+
+        # Search by href
+        link = soup.find('a', href=lambda href: href and keyword in href.lower())
+        if link:
+            contact_us_url = get_full_url(url, link['href'])
+            return contact_us_url
+
+    return None
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def get_privacy_policy_link(soup, url):
     # Try to find the privacy policy URL
@@ -143,15 +198,34 @@ def get_privacy_policy_link(soup, url):
             privacy_policy_url = get_full_url(url, link['href'])
             return privacy_policy_url        
 
-    return None       
-            
+    return None
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
 def scrape_website(response, url):
+    #pp-1
     print("---------------------------scrap---------------------------")
     soup = BeautifulSoup(response.content, 'html.parser')
+    pp_text = ""
+    owner_info = ""
+    
+    for tag in soup.find_all(['p', 'div']):
+        if 'privacy' in tag.text.lower():
+           pp_text += tag.get_text(separator=" ", strip=True) + " "
+        if 'policy' in tag.text.lower():
+           pp_text += tag.get_text(separator=" ", strip=True) + " "
+        
+# Analyze the extracted text
+    if pp_text:
+        owner_info += extract_owner_from_text(pp_text) + "\n"
+        print(f"from privacy policy text:{owner_info}")
+        print("--------------------------------------------------------------------------------")
+        
+    #pp-2
+    pp_url = ""   
     pp_url = get_privacy_policy_link(soup, url)
     print(pp_url)
     print()
-    owner_info = ""
     
    # Scrape and analyze privacy policy if found
     if pp_url:
@@ -159,20 +233,19 @@ def scrape_website(response, url):
             response = requests.get(pp_url, headers=headers, timeout=10)
             if response.status_code == 200:
                 soup = BeautifulSoup(response.content, 'html.parser')
-                print("privacy policy done")
+                
                 privacy_policy_text = soup.get_text(separator=" ", strip=True)
                 owner_info += extract_owner_from_text(privacy_policy_text) + "\n"
-            else:
-                print("fail in detecting privacy text")
-                owner_info =  extract_owner_from_url(row)
+                print(f"privacy policy link:{owner_info}")
+                print("--------------------------------------------------------------------------------")
+                
         except RequestException as e:
-            print(f" policy not reachable")
-            owner_info = extract_owner_from_url(row)
-            print(owner_info)
-            print()
+            print("privacy policy link not reachable")
+            print("--------------------------------------------------------------------------------")
             
 
     # Extract text from the main page (About Us, Contact Us)
+    #au+cu-1        
     about_us_text = ""
     contact_text = ""
     for tag in soup.find_all(['p', 'div']):
@@ -180,15 +253,64 @@ def scrape_website(response, url):
             about_us_text += tag.get_text(separator=" ", strip=True) + " "
         if 'contact' in tag.text.lower():
             contact_text += tag.get_text(separator=" ", strip=True) + " "
-    
+        
     # Analyze the extracted text
     if about_us_text:
         owner_info += extract_owner_from_text(about_us_text) + "\n"
+        print(f"from about us text:{owner_info}")
+        print("--------------------------------------------------------------------------------")
     if contact_text:
         owner_info += extract_owner_from_text(contact_text) + "\n"
-    
-    
+        print(f"from contact us text:{owner_info}")
+        print("--------------------------------------------------------------------------------")
+        
+    #au+cu-2   
+    au_url = get_about_us_link(soup, url)
+    if au_url:
+        try:
+            response = requests.get(au_url, headers=headers, timeout=10)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                about_us_text = soup.get_text(separator=" ", strip=True)
+                owner_info += extract_owner_from_text(about_us_text) + "\n"
+                print(f"from about us link:{owner_info}")
+                print("--------------------------------------------------------------------------------")
+                print()
+                
+        except RequestException as e:
+            print("about us link not reachable")
+            print("--------------------------------------------------------------------------------")
+            print()
+  
+    cu_url = get_contact_us_link(soup, url)
+    if cu_url:
+        try:
+            response = requests.get(cu_url, headers=headers, timeout=10)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                contact_us_text = soup.get_text(separator=" ", strip=True)
+                owner_info += extract_owner_from_text(contact_us_text) + "\n"
+                print(f"from contact us link:{owner_info}")
+                print("--------------------------------------------------------------------------------")
+                print()
+                
+        except RequestException as e:
+            print("conact us link not reachable")
+            print("--------------------------------------------------------------------------------")
+            print()
+
+    footer = soup.find('footer')
+    if footer:
+        # Extract text from the footer
+        footer_text = footer.get_text(separator=' ', strip=True)
+        print("Footer Text:")
+        owner_info += extract_owner_from_text(footer_text) + "\n"
+        print(f"from footer :{owner_info}")
+        print("--------------------------------------------------------------------------------")
+        print()
+  
     return owner_info
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 for row in array_100_rows:
@@ -207,60 +329,49 @@ for row in array_100_rows:
         if response.status_code == 200:
             owner_info = scrape_website(response, url)
             counter += 1
-            if owner_info.strip() == "":
-                print("owner_info is empty and respone = 200")
-                owner_info = extract_owner_from_url(url)
-                print(owner_info)
-                print()
-            else:
-                print(f"Success through text{owner_info}")
-                print()
+            print(f"Success through web scrap and text analysis{owner_info}")
+            print("--------------------------------------------------------------------------------")
+                
         else:
-            counter += 1
-            print(f"Fail 404{counter}")
+            print("Fail 404")
             owner_info = extract_owner_from_url(url)
-            print(owner_info)
+            print(f"Success through url because couldn't visit website{owner_info}")
+            print("--------------------------------------------------------------------------------")
             print()
+            
     except RequestException as e:
         domain = ""
         owner_info2 = ""
+        counter2 += 1
         
-        counter += 1
-        print(f"Exception {counter}")
+        print(f"Exception{counter2}")
         print(url)
         domain = extract_main_domain(url)
         print(f"domain after edit = {domain}")
         
         try:
-            response = requests.get(url, headers=headers, timeout=10)  # Added timeout
+            response = requests.get(domain, headers=headers, timeout=10)  # Added timeout
             # Check the response status
             if response.status_code == 200:
                 owner_info = scrape_website(response, domain)
                 counter += 1
-                if owner_info.strip() == "":
-                    print("response.status_code == 200 and after redo because it is empty")
-                    owner_info = extract_owner_from_url(url)
-                    owner_info  += extract_owner_from_url(domain)
-                    print(owner_info)
-                    print()
-                else:
-                    print(f"owner_info before redo{owner_info}")
-                    print(owner_info)
-                    print()
+                print(f"Success through web scrap and text analysis after extract main domain{owner_info}")
+                print("--------------------------------------------------------------------------------")
             else:
-                counter += 1
-                print(f"Fail 404{counter}")
-                owner_info = extract_owner_from_url(url)
-                owner_info  += extract_owner_from_url(domain)
-                print(owner_info)
-                print()
+                print("Fail 404")
+                owner_info = extract_owner_from_url(domain)
+                print(f"Success through url because couldn't visit website after extract main domain{owner_info}")
+                print("--------------------------------------------------------------------------------")
+                
         except RequestException as e:
-            print("last exception")
+            print("Exception for extracted domain")
             owner_info = extract_owner_from_url(url)
-            owner_info2  = extract_owner_from_url(domain)
-            if len(owner_info2) > 13:
-                print(owner_info)
-            else:
-                print(owner_info)
-                print(owner_info2)
-            print()
+            print(f"Success through url as it is, sent to GPT{owner_info}")
+            print("--------------------------------------------------------------------------------")
+    
+    print("Give GPT a try")
+    owner_info = extract_owner_from_url(url)
+    owner_info += extract_owner_from_url(domain)
+    print(f"Success through url and domain as it is, sent to GPT{owner_info}")
+    print("--------------------------------------------------------------------------------")
+
